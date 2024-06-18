b561bd5351cdb51e9a44527091a500a50f5cebbe
JulinBruto <julioleite93d@gmail.com>
2024-06-18 19:49:49 -0300

    finalizado parte 2

--- src/repositories/wallet-repository.ts
+++ src/repositories/wallet-repository.ts
@@ -4,14 +4,14 @@ import Wallet from "../models/wallet-model";
 const walletRepository = {
     addNew: (wallet: Wallet, callback: (id?: number) => void) => {
         console.log(wallet);
-        const sql = "INSERT INTO Wallets (userId, balance) VALUES (?, ?)";
+        const sql = "INSERT INTO Wallet (userId, balance) VALUES (?, ?)";
         const params = [wallet.userId, wallet.balance];
         database.run(sql, params, function (_err) {
             callback(this?.lastID);
         });
     },
     getById: (id: number, callback: (wallet?: Wallet) => void) => {
-        const sql = "SELECT * FROM Wallets WHERE id = ?";
+        const sql = "SELECT * FROM Wallet WHERE id = ?";
         const params = [id];
         database.get(sql, params, (_err, row) => {
             const wallet = row as Wallet;
